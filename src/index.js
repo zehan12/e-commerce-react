a19d09cc570c42f26f99a901b3f938d59ee5743a
Zehan Khan <zehan9211@gmail.com>
2022-09-28 15:36:13 +0530

    navbar works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import App from './components/App';
+import Contact from './components/Contact';
+import BaseLayout from './components/BaseLayout';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>
+
+, document.getElementById('root'));
