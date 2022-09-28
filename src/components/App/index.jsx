064e0913e464f838b314a8cfca48d46c51580b1a
Zehan Khan <zehan9211@gmail.com>
2022-09-28 15:53:42 +0530

    layout added

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,30 +1,13 @@
-import React, { Component } from 'react';
-import BaseLayout from '../BaseLayout';
-import Items from '../Items';
-import Cart from '../Cart';
+// Dependencies
+import React from 'react';
+//Internals
+import Products from '../Items';
+import './index.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      items: [],
-    }
-  }
-
-  addItemToCart = (item) => {
-    const { items } = this.state;
-    items.push(item);
-    this.setState({items});
-  }
-
-  render() {
-    return(
-      <div>
-        <Items addItemToCart = {this.addItemToCart} />
-        <Cart items = {this.state.items}/>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className="content">
+    <Products />
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './index.css';
-import ReactDOM from 'react-dom';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from './components/App';
-import Contact from './components/Contact';
+import Cart from './components/Cart';
 import BaseLayout from './components/BaseLayout';
+import Women from './components/Women';
+import Men from './components/Men';
+import Clothes from './components/Clothes';
+import Accessories from './components/Accessories';
+import ShowProduct from './components/ShowProduct';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
     <BaseLayout>
       <Switch>
         <Route exact path="/" component={App} />
-        <Route path="/contact" component={Contact} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/women" component={Women}  />
+        <Route path="/men" component={Men} />
+        <Route path="/clothes" component={Clothes}  />
+        <Route path="/accessories" component={Accessories} />
+        <Route exact path="/products/:id" component={ShowProduct} />
       </Switch>
     </BaseLayout>
   </BrowserRouter>
